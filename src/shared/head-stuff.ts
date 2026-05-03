d468af10a4243a18ba628000ca7a465bf115f0d4
Hyz-sui <86903430+Hyz-sui@users.noreply.github.com>
2026-05-03 16:38:00 +0900

    feat: use short app name

--- src/shared/head-stuff.ts
+++ src/shared/head-stuff.ts
@@ -1,9 +1,9 @@
 export const generateTitleBySearchQuery = (query: string | undefined) => {
-  return query ? `「${query}」をストアで探す - honSTOREet` : "お店を選んで本を買おう - honSTOREet";
+  return query ? `「${query}」をストアで探す - honst` : "お店を選んで本を買おう - honst";
 };
 
 export const generateDescriptionBySearchQuery = (query: string | undefined) => {
   return query
-    ? `好きなネット書店、リアル書店、電子書籍ストアを選んで「${query}」を買おう！ honSTOREetは、ワードを指定して各ネット書店/電子書籍ストアの検索・リアル書店の在庫検索のリンクを一覧できるアプリです。`
-    : `honSTOREetは、ワードを指定して各ネット書店/電子書籍ストアの検索・リアル書店の在庫検索のリンクを一覧できるアプリです。`;
+    ? `好きなネット書店、リアル書店、電子書籍ストアを選んで「${query}」を買おう！ honstは、ワードを指定して各ネット書店/電子書籍ストアの検索・リアル書店の在庫検索のリンクを一覧できるアプリです。`
+    : `honstは、ワードを指定して各ネット書店/電子書籍ストアの検索・リアル書店の在庫検索のリンクを一覧できるアプリです。`;
 }
